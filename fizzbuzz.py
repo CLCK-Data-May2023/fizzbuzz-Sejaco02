a40afc58f01dbc6c1e11385e5d79b29e7ddb6802
Seth Jacobs <sejaco02@gmailo.com>
2023-07-19 11:22:48 -0400

    Updated FizzBuzz Assignment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,13 @@
-# add your code here
+# Seth Jacobs Fizz (divisiable by 3) Buzz (divisable by 5) challenge
+for i in range(1,100,1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+
 
